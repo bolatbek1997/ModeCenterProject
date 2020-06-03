011238f53d48bdcae68eb7e2109e756268c89afb
Баке <@DESKTOP-PMUE0ML>
2020-06-04 00:22:57 +0600

    Добавьте файлы проекта.

---
+++ DataProject/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using DataProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProject
+{
+    public partial class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+            : base("name=Task")
+        {
+
+        }
+        public virtual DbSet<Doctor> Doctors { get; set; }
+        public virtual DbSet<UserModel> Users { get; set; }
+        //public virtual DbSet<Film> Films { get; set; }
+        //public virtual DbSet<UserModel> Users { get; set; }
+
+    }
+}
---
+++ DataProject/Models/UserModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProject.Models
+{
+    [Table("UserModel")]
+    public class UserModel
+    {
+        [Key]
+        [Column(Order = 1)]
+        public int Id { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public static UserModel GetUserByLogin(string login)
+        {
+            var db = new ApplicationContext();
+            try
+            {
+                return db.Users.FirstOrDefault(x => x.Login == login);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
